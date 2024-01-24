a9969285228011bec3c3e26c9c7c828705babaef
sebin kim <ksebin96@gmail.com>
2024-01-24 16:06:21 +0900

    수정 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  },
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
